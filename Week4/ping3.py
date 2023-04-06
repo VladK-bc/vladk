7fbea3a7f2bfe0e07d4d49b0669c9dca2be6527a
vlad <krasnovskyv@gmail.com>
2023-04-05 21:12:58 -0700

    week 4 finished

--- Week4/ping3.py
+++ Week4/ping3.py
@@ -1,50 +1,57 @@
 #!/usr/bin/python3
 #VladK ping3
 
-import subprocess
+
 import sys
-import pinglib
-import os
+import subprocess
 import csv
-import io
-from contextlib import redirect_stdout
+import socket
+import re
 
-if len(sys.argv) < 2:
-    arg = sys.argv[1]
-    if os.path.isfile(arg):
-            filename = sys.argv[1]
-
-            with open(filename) as f:
-                for line in f:
-                    ip_address  =line.strip()
-                    pinglib.pingthis(ip_address)
-                   
-    else:
-        ip_address = sys.argv[1]
-        pinglib.pingthis(ip_address)
+def ping_addresses(addresses, output_file=None):
+    results = []
+#checks ip addresses and throws error code
+    for address in addresses:
+        try:
+            response = subprocess.check_output(['ping', '-c', '1', address], stderr=subprocess.DEVNULL)
+            response = response.decode('utf-8')
+            match = re.search(r'time=(\d+(?:\.\d+)?) ms', response)
+            if match:
+                time_to_ping = match.group(1)
+                result = f"{address}, {time_to_ping}"
+            else:
+                result = f"{address}, Not found"
+        except subprocess.CalledProcessError:
+            result = f"{address}, Not found"
         
+        results.append(result)
+#prints output
+    print("IP,TimeToPing(ms)")
+    for result in results:
+        print(result)
 
-if len(sys.argv) > 2:
-      csv_file = sys.argv[2]
-      arg = sys.argv[1]
-      if os.path.isfile(arg):
-            filename = sys.argv[1]
+    if output_file:
+        with open(output_file, 'w', newline='') as csvfile:
+            csvfile.write("IP,TimeToPing(ms)\n")
+            for result in results:
+                csvfile.write(result + "\n")
+#calls main
+def main():
+    input_arg = sys.argv[1]
+    output_file = sys.argv[2] if len(sys.argv) > 2 else None
 
-            with open(filename) as f:
-                for line in f:
-                    ip_address  =line.strip()
-                    result = pinglib.pingthis(ip_address)
-                    with open(csv_file, 'w', newline='') as csvfile:
-                         writer = csv.writer(csvfile)
-                         writer.writerow(['Result'])
-                         writer.writerow([result])
-                         
-      else:
-             ip_address = sys.argv[1]
-             pinglib.pingthis(ip_address)
-             result = pinglib.pingthis(ip_address)
-             with open(csv_file, 'w', newline='') as csvfile:
-                         writer = csv.writer(csvfile)
-                         writer.writerow(['Result'])
-                         writer.writerow([result])
-                       
+    addresses = []
+#appends csv file
+    try:
+        socket.inet_aton(input_arg)
+        addresses.append(input_arg)
+    except socket.error:
+        with open(input_arg, 'r') as file:
+            for line in file.readlines():
+                line = line.strip()
+                addresses.append(line)
+
+    ping_addresses(addresses, output_file)
+
+if __name__ == '__main__':
+    main()

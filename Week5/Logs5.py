348d21c905def7f5d579ce9d9f3d0c92c0735558
vlad <krasnovskyv@gmail.com>
2023-04-20 23:50:58 -0700

    Week5 and Week6 Complete

--- Week5/Logs5.py
+++ Week5/Logs5.py
@@ -5,33 +5,30 @@
 
 import re
 import csv
+from collections import defaultdict
 
 input_file = "wafeventlog"
 output_file = "redirect.csv"
 
-count_pattern = re.compile(r"\[id \"(\d+)\"\]")
-from_pattern = re.compile(r"\[client (\d+\.\d+\.\d+\.\d+)\]")
-to_pattern = re.compile(r"\[hostname \"(\d+\.\d+\.\d+\.\d+)")
-exclude_term = "ActiveSync"
+redirect_pattern = re.compile(r'\[msg "REDIRECT: (.*?) to (.*?)"]')
+exclude_terms = ["ActiveSync", "Basic"]
 
 with open(input_file, "r") as file:
     lines = file.readlines()
 
-results = []
+redirects = defaultdict(int)
 
 for line in lines:
-    if exclude_term not in line:
-        count_match = count_pattern.search(line)
-        from_match = from_pattern.search(line)
-        to_match = to_pattern.search(line)
+    if not any(term in line for term in exclude_terms):
+        redirect_match = redirect_pattern.search(line)
 
-        if count_match and from_match and to_match:
-            count = count_match.group(1)
-            from_ip = from_match.group(1)
-            to_ip = to_match.group(1)
-            results.append((count, from_ip, to_ip))
+        if redirect_match:
+            from_domain = redirect_match.group(1)
+            to_url = redirect_match.group(2)
+            redirects[(from_domain, to_url)] += 1
 
 with open(output_file, "w", newline="") as csvfile:
     csvwriter = csv.writer(csvfile)
     csvwriter.writerow(["Count", "From", "To"])
-    csvwriter.writerows(results)
+    for (from_domain, to_url), count in redirects.items():
+        csvwriter.writerow([count, from_domain, to_url])
--- Week6/nmap2.py
+++ Week6/nmap2.py
@@ -2,14 +2,14 @@
 #VladK nmap2
 
 import csv
-import nmap
+import nmap3a
 
 def read_ips(file):
     with open(file, newline='') as f:
         return [row['IP'] for row in csv.DictReader(f)]
 
 def scan_os(ip):
-    scanner = nmap.PortScanner()
+    scanner = nmap3a.PortScanner()
     scanner.scan(hosts=ip, arguments='-O')
     open_ports = [str(port) for port in scanner[ip]['tcp'].keys() if scanner[ip]['tcp'][port]['state'] == 'open']
     os_family = scanner[ip].get('osclass', {}).get('osfamily', 'Unknown')

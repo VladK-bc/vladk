90f8fc9e24fb3b9721fbd59d05b6f167ad557f66
vlad <krasnovskyv@gmail.com>
2023-04-21 01:13:36 -0700

    Week6 Complete

--- Week6/nmap2.py
+++ Week6/nmap2.py
@@ -2,18 +2,23 @@
 #VladK nmap2
 
 import csv
-import nmap3a
+import nmap3
 
 def read_ips(file):
     with open(file, newline='') as f:
         return [row['IP'] for row in csv.DictReader(f)]
 
-def scan_os(ip):
-    scanner = nmap3a.PortScanner()
-    scanner.scan(hosts=ip, arguments='-O')
-    open_ports = [str(port) for port in scanner[ip]['tcp'].keys() if scanner[ip]['tcp'][port]['state'] == 'open']
-    os_family = scanner[ip].get('osclass', {}).get('osfamily', 'Unknown')
-    return {'IP': ip, 'OpenPorts': ','.join(open_ports), 'OSType': os_family}
+def scan_os_and_ports(ip):
+    nmap = nmap3.Nmap()
+    os_results = nmap.nmap_os_detection(ip)
+    os_family = 'Unknown'
+    if os_results and isinstance(os_results[ip]['osmatch'], list) and os_results[ip]['osmatch'][0].get('osclass'):
+        os_family = os_results[ip]['osmatch'][0]['osclass'].get('osfamily', 'Unknown')
+    else:
+        print(f"OS detection results for IP {ip}: {os_results}")
+    open_ports = nmap.scan_top_ports(ip)
+    open_ports_list = ','.join([str(port_info['portid']) for port_info in open_ports[ip]['ports']])
+    return {'IP': ip, 'OpenPorts': open_ports_list, 'OSType': os_family}
 
 def write_csv(data, file):
     with open(file, 'w', newline='') as f:
@@ -26,7 +31,7 @@ def main():
     output_csv = 'nmap2.csv'
 
     ip_list = read_ips(input_csv)
-    scanned_data = [scan_os(ip) for ip in ip_list]
+    scanned_data = [scan_os_and_ports(ip) for ip in ip_list]
 
     write_csv(scanned_data, output_csv)
 
